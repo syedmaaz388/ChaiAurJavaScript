9f5302e9cc8845907853b89f4305b010ac6c6e8e
Syed Maaz <63224539+syedmaaz388@users.noreply.github.com>
2023-08-24 21:46:29 +0530

    Add files via upload

---
+++ Basics/02DataTypesAndEcma.js
@@ -0,0 +1,26 @@
+// ECMA 
+// What is JavaScript vs ECMAScript vs ES6?
+// JavaScript ES6 (also known as ECMAScript 2015 or ECMAScript 6) is the newer version of JavaScript that was introduced in 2015.
+//  ECMAScript is the standard that JavaScript programming language uses. 
+// ECMAScript provides the specification on how JavaScript programming language should work.
+
+// DataTypes - Primitive
+// String
+// Number
+// Boolean
+// BigInt
+// Symbol
+// null - Standalone value
+// undefined
+
+// Not Primitive
+// Objects
+
+console.log(typeof "Maaz")
+console.log(typeof 23)
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
+
+
+
