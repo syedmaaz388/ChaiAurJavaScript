8d5744e608d62830ef1ff1c97acc76c923b38ca6
Syed Maaz <63224539+syedmaaz388@users.noreply.github.com>
2023-08-24 20:49:33 +0530

    Add files via upload

---
+++ Basics/01Variables.js
@@ -0,0 +1,21 @@
+const accountID = "112133";
+let name = "Syed Maaz";
+var emailId = "maaz@gmail.com";
+usercity = "Jhansi"
+
+// accountID = "142233"; not allowed
+
+// let name = "Maaz" not allowed redeclared nahi hoga
+
+name = "Syed"; // reassign ho jaaega
+emailId = "Syed@gmail.com";
+usercity = "Dehli";
+
+// let is a blocked Scope
+// Var is a function Scope
+// Aage paregai iske baare mai
+
+console.table([accountID,name,emailId,usercity]);
+
+// Prefer not to use var
+
